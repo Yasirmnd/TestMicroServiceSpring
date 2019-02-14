a2a7667c59be8d12df9aa60049b60c90d46e51e6
yasirmnd <yasirmnd@gmail.com>
2019-02-14 13:06:07 -0500

    Test from eclipse

--- src/main/java/application/SBApplication.java
+++ src/main/java/application/SBApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SBApplication {
 
     public static void main(String[] args) {
+    	
         SpringApplication.run(SBApplication.class, args);
     }
 }
