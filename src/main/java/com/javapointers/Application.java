be48c7f3f86ca046e6bfdf5b1fb65bf3ce49d907
Yasirmnd <yasirmnd@gmail.com>
2019-02-14 23:42:26 -0500

    Update Application.java

--- src/main/java/com/javapointers/Application.java
+++ src/main/java/com/javapointers/Application.java
@@ -11,6 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
     public static void main(String[] args) {
+        system.out.println("Hello Testing");
         SpringApplication.run(Application.class, args);
     }
 }
